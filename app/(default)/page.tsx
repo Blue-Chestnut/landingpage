65a7be381be3b160e2c084619bb13c7cacd55678
Matze99 <hamachermatthias12@gmail.com>
2025-01-30 14:22:59 +0100

    removed unused imports

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -5,8 +5,6 @@ export const metadata = {
 
 import BusinessCategories from "@/components/business-categories";
 import FeaturesPlanet from "@/components/features-planet";
-import LargeTestimonial from "@/components/large-testimonial";
-import Cta from "@/components/cta";
 import HeroEverBlue from "@/components/hero-everblue";
 
 export default function Home() {
